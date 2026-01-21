cde11146dea468a3527f95670ae815ba7fd69545
RAKIB2699 <rakibhasan01645172699@gmail.com>
2026-01-22 01:14:27 +0600

    server setup and post api created

---
+++ src/server.ts
@@ -0,0 +1,79 @@
+import express, { Request, Response } from "express";
+import { Pool } from "pg";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+const app = express()
+const port = 5000
+
+app.use(express.json());
+
+const pool = new Pool({
+    connectionString: process.env.CONNECTION_STR
+})
+
+
+
+const initDB = async () => {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(150) NOT NULL,
+        email VARCHAR(150) UNIQUE NOT NULL,
+        age INT,
+        phone VARCHAR(15),
+        address TEXT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        ) `
+    )
+
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS todos(
+        id SERIAL PRIMARY KEY,
+        user_id INT REFERENCES users(id) ON DELETE CASCADE,
+        title VARCHAR(150) NOT NULL,
+        description TEXT,
+        completed BOOLEAN DEFAULT false,
+        due_date DATE,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `)
+}
+initDB();
+
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello billai!')
+})
+
+app.post('/users', async (req: Request, res: Response) => {
+    const { name, email } = req.body;
+
+    try {
+        const result = await pool.query(`INSERT INTO users(name,email) VALUES($1,$2) RETURNING *`, [name, email]);
+
+        res.status(201).json({
+            success: true,
+            meassage: "data inserted",
+            data: result.rows[0]
+        });
+
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            meassage: "data not inserted"
+        });
+    };
+
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})

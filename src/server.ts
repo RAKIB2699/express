165015e3197bf5be715333e53354d33e576cb565
RAKIB2699 <rakibhasan01645172699@gmail.com>
2026-01-22 13:53:43 +0600

    put and delete api create

--- src/server.ts
+++ src/server.ts
@@ -100,7 +100,7 @@ app.get("/users/:id", async (req: Request, res: Response) => {
         if (result.rows.length == 0) {
             res.status(500).json({
                 success : false,
-                message : "lengh nehi ha"
+                message : "failed data retrived"
             })
         } else {
             res.status(200).json({
@@ -114,6 +114,60 @@ app.get("/users/:id", async (req: Request, res: Response) => {
         res.status(500).json({
             success: false,
             message: "data retrived failed"
+        })
+    }
+})
+
+
+app.put("/users/:id", async (req: Request, res: Response) => {
+    
+    const {name,email} = req.body;
+    try {
+        const result = await pool.query(`UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *`,[name,email,req.params.id]);
+
+        if (result.rows.length == 0) {
+            res.status(500).json({
+                success : false,
+                message : "failed data update"
+            })
+        } else {
+            res.status(200).json({
+                success: true,
+                message: "data update successfully",
+                data: result.rows[0]
+            })
+        }
+
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: "data update failed"
+        })
+    }
+})
+
+app.delete("/users/:id", async (req: Request, res: Response) => {
+
+    try {
+        const result = await pool.query(`DELETE FROM users WHERE id = $1 RETURNING *`, [req.params.id]);
+
+        if (result.rows.length == 0) {
+            res.status(500).json({
+                success : false,
+                message : "user not found"
+            })
+        } else {
+            res.status(200).json({
+                success: true,
+                message: "data delete successfully",
+                data: result.rows[0]
+            })
+        }
+
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: "data delete failed"
         })
     }
 })
